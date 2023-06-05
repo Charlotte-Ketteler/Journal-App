52529534b3f0eef0c3556f7a68014fe2f1e76326
Charlotte Ketteler <ketteler.cha@gmail.com>
2023-06-05 10:06:28 +0200

    submit-form

--- src/App.js
+++ src/App.js
@@ -5,14 +5,46 @@ import Form from './components/JS/Form';
 import Head from './components/JS/Head';
 import Entry from './components/JS/Entry';
 import Footer from './components/JS/Footer';
+import {useState} from "react";
+
+
+const entries = [
+   
+  {
+    id: 125,
+    date: "03.06.2023",
+    motto: "My favourite drink",
+    notes: "Moscow Mule"
+  },
+  {
+    id: 124,
+    date:"02.06.2023",
+    motto: "Festivals I want to go to",
+    notes:"Cochella, Parookaville and Tomorrowland"
+  },
+  {
+    id: 123,
+    date:"01.06.2023",
+    motto: "Colognes constitutional law",
+    notes:"§ 1 - Et es wie et es § 2 - Et kütt wie et kütt § 3 - Et hätt noch immer jot jejange"
+  }
+]
+
+
 function App() {
+  const [entry, setEntry] = useState(entries);
+  
+  function handleAddEntries(newEntry){
+  
+    setEntry([...entry, {...newEntry}]);
+  }
   return (
     <>
      
       <main className='typo__wholePage'>
       <Head/>
-      <Form name="Motto" value=""></Form>
-      <Entry></Entry>
+      <Form onAddEntry={handleAddEntries}></Form>
+      <Entry entryProp={entry}></Entry>
       </main>
       <Footer></Footer>
 
--- src/components/JS/Form.js
+++ src/components/JS/Form.js
@@ -1,21 +1,32 @@
 import "../CSS/Form.css";
+import "../CSS/Button.css";
 
-export default function Form({ name, value}){
+export default function Form({ onAddEntry}){
+    
+    function handleSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
+        onAddEntry(data);
+        console.log("hi");
+    }
     return (
     <div>
     <h2 className="position__H2--Form">NEW ENTRY</h2>
-    <form className="position__Form">
+    <form className="position__Form" onSubmit={handleSubmit}>
         <p>Motto</p>
         <label htmlFor="Motto"></label>
-        <input className="form__Style--Motto" id="Motto" name={name} defaultValue={value}/>
+        <input className="form__Style--Motto" id="Motto"  type="text" name="motto" placeholder="motto"/>
         <br/>
         <p>Notes</p>
         <label htmlFor="text"></label>
-        <textarea className="form__Style--Notes" id="text"/>
+        <textarea name="notes" className="form__Style--Notes" id="text"/>
         <br/>
+        <button className="create__button" type="submit"  ><p>Create</p></button>
         </form>
         
          </div>
         )
        
 }
+ 
